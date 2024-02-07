be671ecc71903629995a54be856bd2fda1f5e5c2
Pablo Cruz Ramos <A01138740@itesm.mx>
2024-02-06 23:25:23 -0800

    working on responsiveness of sections

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -8,7 +8,28 @@ const Home = () => {
  return (
   <div className="flex relative h-[90vh] z-10 text-black justify-center border-t-gray-200 border-t-2">
    <SectionGuidelines />
-   <div className="relative h-40 p-24"></div>
+   <div className="relative flex flex-col lg:flex-row pt-16 pb-6 px-24">
+    <div className="w-3/5 pt-16 pb-5">
+     <div className="hero-content">
+      <div className="text-black">
+       <h1 className="text-6xl font-bold pb-3">Acelera tu oficina</h1>
+       <h1 className="text-6xl font-bold pb-1">Eleva tu productividad</h1>
+       <p className="py-6 text-2xl mr-32">
+        Descubre un nuevo nivel de eficiencia con nuestras soluciones de
+        impresión y copiadoras - Desde impresoras multifunción hasta copiadoras
+        de alta calidad.
+       </p>
+       <button className="btn bg-red-500 font-bold text-white">
+        Conocer Servicios
+       </button>
+       <button className="btn font-bold text-red">Contáctanos</button>
+      </div>
+     </div>
+    </div>
+    <div className="w-2/5 hero text-black items-start">
+     <img src={printer} className="h-5/6"></img>
+    </div>
+   </div>
   </div>
   //   <div className="h-auto font-ibmplex bg-gray-50">
   //    <div className="flex flex-col lg:flex-row pt-16 pb-6 px-24">
