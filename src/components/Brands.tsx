568160db93895c9c8015f5bf46da5b2abdf5eca0
Pablo Cruz Ramos <A01138740@itesm.mx>
2024-02-09 18:58:46 -0800

    termine componente de Brands

--- src/components/Brands.tsx
+++ src/components/Brands.tsx
@@ -12,23 +12,23 @@ const Brands = () => {
     heightL={150}
     color="bg-gray-50"
    />
-   <div className="relative w-[100%] flex flex-col px-4 justify-center">
-    <div className="text-center font-semibold font-ibmplex text-2xl pb-3">
+   <div className="relative w-[100%] flex flex-col px-4 justify-center py-10">
+    <div className="text-center font-semibold font-ibmplex text-2xl pb-6">
      <h1>{"Contamos con las mejores marcas"}</h1>
     </div>
     <div className="relative flex flex-col lg:flex-row px-4 items-center">
-     <div className="w-1/2 h-[70%] flex flex-col lg:flex-row gap-12 items-center">
+     <div className="w-1/2 h-[70%] flex flex-col lg:flex-row gap-3 lg:gap-10 items-center text-center lg:text-start pb-6">
       <img src={ricohLogo} className="w-[100px] h-[20px]"></img>
       <div>
-       Aqui va el textAqui va el textAqui va el textAqui va el textAqui va el
-       textAqui va el text
+       Corporación global reconocida por su amplia gama de soluciones de
+       impresión, gestión documental y servicios digitales innovadores.
       </div>
      </div>
-     <div className="w-1/2 h-[70%] flex flex-col lg:flex-row gap-12 items-center">
+     <div className="w-1/2 h-[70%] flex flex-col lg:flex-row gap-3 lg:gap-10 items-center text-center lg:text-start pb-6">
       <img src={sharpLogo} className="w-[100px] h-[20px]"></img>
       <div>
-       Aqui va el textAqui va el textAqui va el textAqui va el textAqui va el
-       textAqui va el text
+       Empresa líder en innovación tecnológica y diseño en una variedad de
+       productos electrónicos y soluciones tecnológicas.
       </div>
      </div>
     </div>
